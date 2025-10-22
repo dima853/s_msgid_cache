5a7551be513092751951950a15d0710f2a3b86b6
dima853 <174457042+dima853@users.noreply.github.com>
2025-10-22 10:28:13 +0300

    Update constants.c

--- constants.c
+++ constants.c
@@ -1,5 +1,5 @@
 #include "s_msgid_cache.h"
 
-const uint8_t DEFAULT_CAPACITY = 1000;
+const size_t DEFAULT_CAPACITY = 1000;
 const time_t CLEANUP_INTERVAL = 300;   // 5 минут
 const time_t ENTRY_TTL = 1800;         // 30 минут
