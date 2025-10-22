2fd36aa590e1d3ad6fded1687c4096cd61c80d78
dima853 <174457042+dima853@users.noreply.github.com>
2025-10-22 10:34:28 +0300

    Update constants.c

--- constants.c
+++ constants.c
@@ -1,5 +1,22 @@
-#include "s_msgid_cache.h"
+#include "constants.h"
 
-const size_t DEFAULT_CAPACITY = 1000;
-const time_t CLEANUP_INTERVAL = 300;   // 5 минут
-const time_t ENTRY_TTL = 1800;         // 30 минут
+static const size_t DEFAULT_CAPACITY = 100;
+static const time_t CLEANUP_INTERVAL = 300;    // 5 минут
+static const time_t ENTRY_TTL = 3600;          // 1 час  
+static const uint8_t MAX_RETRIES = 3;
+
+size_t get_default_capacity(void) {
+    return DEFAULT_CAPACITY;
+}
+
+time_t get_cleanup_interval(void) {
+    return CLEANUP_INTERVAL;
+}
+
+time_t get_entry_ttl(void) {
+    return ENTRY_TTL;
+}
+
+uint8_t get_max_retries(void) {
+    return MAX_RETRIES;
+}
